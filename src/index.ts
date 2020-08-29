a5314fef1d71738e5bfb01d433144089db67a241
VCD <VeryCrazyDog@gmail.com>
2020-08-29 09:18:48 +0800

    More implementation

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,21 @@ const LOG_LEVEL_INFO_SYMBOL: unique symbol = Symbol('info')
 const LOG_LEVEL_WARN_SYMBOL: unique symbol = Symbol('warn')
 const LOG_LEVEL_ERROR_SYMBOL: unique symbol = Symbol('error')
 const TIMESTAMP_SYMBOL: unique symbol = Symbol('timestamp')
+const LOG_LEVEL_SYMBOL: unique symbol = Symbol('log-level')
 
-type LogLevelConfigurable = 'disabled' | 'trace' | 'debug' | 'info' | 'warn' | 'error'
-type LogLevelText = Exclude<LogLevelConfigurable, 'disabled'>
+type LogLevelText = 'trace' | 'debug' | 'info' | 'warn' | 'error'
 type LogLevelSymbol =
   typeof LOG_LEVEL_TRACE_SYMBOL |
   typeof LOG_LEVEL_DEBUG_SYMBOL |
   typeof LOG_LEVEL_INFO_SYMBOL |
   typeof LOG_LEVEL_WARN_SYMBOL |
   typeof LOG_LEVEL_ERROR_SYMBOL
-type TimestampFormatFunction = (value: Date) => string
-type LogFunction = (messageLevel: LogLevelText, prefix?: any, message?: any, ...optionalParams: any[]) => void
+export type LogLevelConfigurable = 'disabled' | LogLevelText
+export type TimestampFormatFunction = (value: Date) => string
+export type LogFunction = (messageLevel: LogLevelText, prefix?: any, message?: any, ...optionalParams: any[]) => void
 type LevelLogFunction = (message?: any, ...optionalParams: any[]) => void
 
-interface LoggerOptions {
+export interface LoggerOptions {
   levelStrict?: LogLevelConfigurable
   level?: string
   prefix?: any[]
@@ -25,13 +26,15 @@ interface LoggerOptions {
   logger?: LogFunction
 }
 
-export const LogLevel: Readonly<{
-  trace: typeof LOG_LEVEL_TRACE_SYMBOL
-  debug: typeof LOG_LEVEL_DEBUG_SYMBOL
-  info: typeof LOG_LEVEL_INFO_SYMBOL
-  warn: typeof LOG_LEVEL_WARN_SYMBOL
-  error: typeof LOG_LEVEL_ERROR_SYMBOL
+export const LogTags: Readonly<{
+  TIMESTAMP: typeof TIMESTAMP_SYMBOL
+  LOG_LEVEL: typeof LOG_LEVEL_SYMBOL
 }> = {
+  TIMESTAMP: TIMESTAMP_SYMBOL,
+  LOG_LEVEL: LOG_LEVEL_SYMBOL
+}
+
+const LOG_LEVEL_TEXT_TO_SYMBOL_MAP: Record<LogLevelText, LogLevelSymbol> = {
   trace: LOG_LEVEL_TRACE_SYMBOL,
   debug: LOG_LEVEL_DEBUG_SYMBOL,
   info: LOG_LEVEL_INFO_SYMBOL,
@@ -58,3 +61,25 @@ const DEFAULT_LOGGERS: Record<LogLevelSymbol, LevelLogFunction> = {
 }
 
 function noop (): void {}
+
+export default class LevelLogger {
+  #level: LogLevelConfigurable
+  #logger: LogFunction | null
+
+  constructor (options?: LoggerOptions) {
+    this.#level = 'info'
+    this.#logger = null
+    if (options != null) {
+      this.#logger = options.logger ?? null
+      if (options.levelStrict != null) {
+        this.setLevel(options.levelStrict, true)
+      } else if (options.level != null) {
+        this.setLevel(options.level, false)
+      }
+    }
+  }
+
+  setLevel (level: string, strict: Boolean): void {
+    // TODO Implement
+  }
+}
