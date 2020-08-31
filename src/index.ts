64c7820418fdf6a80c5d394b156d2a0195a5a753
VCD <VeryCrazyDog@gmail.com>
2020-08-31 21:31:04 +0800

    Implemented updateLevelLogger()

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,18 @@
-const LOG_LEVEL_TRACE_SYMBOL: unique symbol = Symbol('trace')
-const LOG_LEVEL_DEBUG_SYMBOL: unique symbol = Symbol('debug')
-const LOG_LEVEL_INFO_SYMBOL: unique symbol = Symbol('info')
-const LOG_LEVEL_WARN_SYMBOL: unique symbol = Symbol('warn')
-const LOG_LEVEL_ERROR_SYMBOL: unique symbol = Symbol('error')
+import util from 'util'
+
 const TIMESTAMP_SYMBOL: unique symbol = Symbol('timestamp')
 const LOG_LEVEL_SYMBOL: unique symbol = Symbol('log-level')
 
-type LogLevelText = 'trace' | 'debug' | 'info' | 'warn' | 'error'
-type LogLevelSymbol =
-  typeof LOG_LEVEL_TRACE_SYMBOL |
-  typeof LOG_LEVEL_DEBUG_SYMBOL |
-  typeof LOG_LEVEL_INFO_SYMBOL |
-  typeof LOG_LEVEL_WARN_SYMBOL |
-  typeof LOG_LEVEL_ERROR_SYMBOL
-export type LogLevelConfigurable = 'disabled' | LogLevelText
+type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error'
+export type LogLevelConfigurable = 'disabled' | LogLevel
 export type TimestampFormatFunction = (value: Date) => string
-export type LogFunction = (messageLevel: LogLevelText, prefix?: any, message?: any, ...optionalParams: any[]) => void
+export type LogFunction = (messageLevel: LogLevel, prefix?: any[], message?: any, ...optionalParams: any[]) => void
 type LevelLogFunction = (message?: any, ...optionalParams: any[]) => void
 
 export interface LoggerOptions {
   levelStrict?: LogLevelConfigurable
   level?: string
-  prefix?: any[]
+  prefixes?: any[]
   timestampFormatter?: TimestampFormatFunction
   logger?: LogFunction
 }
@@ -34,13 +25,16 @@ export const LogTags: Readonly<{
   LOG_LEVEL: LOG_LEVEL_SYMBOL
 }
 
-const LOG_LEVEL_TEXT_TO_SYMBOL_MAP: Record<LogLevelText, LogLevelSymbol> = {
-  trace: LOG_LEVEL_TRACE_SYMBOL,
-  debug: LOG_LEVEL_DEBUG_SYMBOL,
-  info: LOG_LEVEL_INFO_SYMBOL,
-  warn: LOG_LEVEL_WARN_SYMBOL,
-  error: LOG_LEVEL_ERROR_SYMBOL
+const LOG_LEVEL_CONFIGURABLE_TO_PRIORITY: Record<LogLevelConfigurable, number> = {
+  trace: 0,
+  debug: 1,
+  info: 2,
+  warn: 3,
+  error: 4,
+  disabled: 5
 }
+const CONFIGURABLE_LOG_LEVELS: LogLevelConfigurable[] = ['trace', 'debug', 'info', 'warn', 'error', 'disabled']
+const LOG_LEVELS: LogLevel[] = ['trace', 'debug', 'info', 'warn', 'error']
 
 const DEFAULT_TIMESTAMP_FORMATTER: TimestampFormatFunction = value => {
   const year = value.getFullYear()
@@ -52,34 +46,115 @@ const DEFAULT_TIMESTAMP_FORMATTER: TimestampFormatFunction = value => {
   return `${year}-${month}-${day} ${hour}:${minute}:${second}`
 }
 
-const DEFAULT_LOGGERS: Record<LogLevelSymbol, LevelLogFunction> = {
-  [LOG_LEVEL_TRACE_SYMBOL]: console.trace,
-  [LOG_LEVEL_DEBUG_SYMBOL]: console.debug,
-  [LOG_LEVEL_INFO_SYMBOL]: console.info,
-  [LOG_LEVEL_WARN_SYMBOL]: console.warn,
-  [LOG_LEVEL_ERROR_SYMBOL]: console.error
+const DEFAULT_LOGGERS: Record<LogLevel, LevelLogFunction> = {
+  trace: console.trace,
+  debug: console.debug,
+  info: console.info,
+  warn: console.warn,
+  error: console.error
 }
 
 function noop (): void {}
 
 export default class LevelLogger {
   #level: LogLevelConfigurable
+  #prefixes: any[]
+  #timestampFormatter: TimestampFormatFunction
   #logger: LogFunction | null
+  #levelLogger: Record<LogLevel, LevelLogFunction>
 
   constructor (options?: LoggerOptions) {
     this.#level = 'info'
+    this.#prefixes = []
+    this.#timestampFormatter = DEFAULT_TIMESTAMP_FORMATTER
     this.#logger = null
+    this.#levelLogger = {
+      ...DEFAULT_LOGGERS
+    }
     if (options != null) {
-      this.#logger = options.logger ?? null
       if (options.levelStrict != null) {
-        this.setLevel(options.levelStrict, true)
+        this.setLevelStrict(options.levelStrict)
       } else if (options.level != null) {
-        this.setLevel(options.level, false)
+        this.setLevel(options.level)
       }
+      if (options.prefixes != null) {
+        this.setPrefixes(options.prefixes)
+      }
+      if (options.timestampFormatter != null) {
+        this.#timestampFormatter = options.timestampFormatter
+      }
+      this.#logger = options.logger ?? null
+    }
+    this.updateLevelLogger()
+  }
+
+  setLevel (level: string): void {
+    const lowerCaseLevel = level.toLowerCase()
+    if ((CONFIGURABLE_LOG_LEVELS as string[]).includes(lowerCaseLevel)) {
+      this.setLevel(lowerCaseLevel as LogLevelConfigurable)
     }
   }
 
-  setLevel (level: string, strict: Boolean): void {
-    // TODO Implement
+  setLevelStrict (level: LogLevelConfigurable): void {
+    this.#level = level
+    this.updateLevelLogger()
+  }
+
+  getLevel (): LogLevelConfigurable {
+    return this.#level
+  }
+
+  setPrefixes (prefixes: any[]): void {
+    this.#prefixes = prefixes
+    this.updateLevelLogger()
+  }
+
+  getPrefixes (): any[] {
+    return [...this.#prefixes]
+  }
+
+  private updateLevelLogger (): void {
+    const levelPriority = LOG_LEVEL_CONFIGURABLE_TO_PRIORITY[this.#level]
+    LOG_LEVELS.forEach(logLevel => {
+      if (levelPriority <= LOG_LEVEL_CONFIGURABLE_TO_PRIORITY[logLevel]) {
+        if (this.#logger == null) {
+          this.#levelLogger[logLevel] = ((messageLevel, prefixes) => {
+            return (message?: any, ...optionalParams: any[]) => {
+              let newMessage: any
+              if (prefixes.length > 0) {
+                if (message !== undefined) {
+                  newMessage = util.format(this.resolveSymbols(prefixes, messageLevel), message)
+                } else {
+                  newMessage = util.format(this.resolveSymbols(prefixes, messageLevel))
+                }
+              } else {
+                newMessage = message
+              }
+              DEFAULT_LOGGERS[messageLevel](newMessage, ...optionalParams)
+            }
+          })(logLevel, this.#prefixes)
+        } else {
+          this.#levelLogger[logLevel] = ((messageLevel, prefixes, logger) => {
+            return (message?: any, ...optionalParams: any[]) => {
+              logger(messageLevel, this.resolveSymbols(prefixes, messageLevel), message, ...optionalParams)
+            }
+          })(logLevel, this.#prefixes, this.#logger)
+        }
+      } else {
+        this.#levelLogger[logLevel] = noop
+      }
+    })
+  }
+
+  private resolveSymbols (data: any[], messageLevel: LogLevel): any[] {
+    return data.map(p => {
+      if (p === TIMESTAMP_SYMBOL) {
+        return this.#timestampFormatter(new Date())
+      } else if (p === LOG_LEVEL_SYMBOL) {
+        return messageLevel
+      } else {
+        return p
+      }
+    })
   }
 }
